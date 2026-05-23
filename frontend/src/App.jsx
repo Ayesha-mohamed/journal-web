0f9dc0dfa4066a29b242d25ac24da3ea46c91651
Ayesha-mohamed <aishamohamedabdullahi950@gmail.com>
2026-05-23 13:18:11 -0700

    update3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import Contact from "./pages/Contact"
 import Articles from "./pages/Articles"
+import Dashboard from "./pages/Dashbord"
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
   <Route path="/" element={<Home />} />
   <Route path="/contact" element={<Contact />} />
   <Route path="/articles" element={<Articles />} />
+  <Route path="/dashboard" element={<Dashboard />} />
 
 </Routes>
 
